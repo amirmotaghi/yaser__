c77183a885c95e3e2926a66f6244877e99cc1af6
amirmotaghi <motaghizadeamir@gmail.com>
2016-06-15 03:16:24 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {166538399,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -235,21 +235,14 @@ An advance Administration bot based on yagop/telegram-bot
 https://github.com/SEEDTEAM/TeleSeed
 
 Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
+Yaser (@pesarak_yas)
 
 Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
+هیچ کس
 
 Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+E
+کانال ما: @shadow_support
 ]],
     help_text_realm = [[
 Realm Commands:
@@ -258,7 +251,7 @@ Realm Commands:
 Create a group
 
 !createrealm [name]
-Create a realm
+ساخت گپ
 
 !setname [name]
 Set realm name
